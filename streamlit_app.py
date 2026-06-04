691d22dc60cb214d9f1e8197017de53af22bfe5e
Pooja-Gupta-WK-dot <poojagupta1987@gmail.com>
2026-06-04 12:54:03 +0530

    Update streamlit_app.py
    connected changes needed bc of move from SIS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
+  """Choose the fruits you want in your custom Smoothie !
   """
 )
  
